6d7acdbbe7c506b8d6c7119d9eb64b8c93af354f
BerAndrade <111024132+BerAndrade@users.noreply.github.com>
2022-10-09 16:04:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace algoritmoeleitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N;
+            Console.WriteLine("***ALGORÍTMO DO ELEITOR***");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("\nPor favor, informe a sua idade: ");
+                N = double.Parse(Console.ReadLine());
+                if (N < 16)
+                    Console.WriteLine("\nMenor de idade, não eleitor.");
+                else
+                    if ((N >= 18) && (N <= 65))
+                    Console.WriteLine("\nMaior de 18 anos, eleitor obrigatório.");
+                else
+                    Console.WriteLine("Eleitor facultativo.");
+            }           
+        }
+    }
+}
